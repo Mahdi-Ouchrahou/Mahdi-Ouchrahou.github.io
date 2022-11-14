8cd700086987e2d3564a43d517f853e364704f00
Mahdi-Ouchrahou <m.ouchrahou@jacobs-university.de>
2022-11-14 23:25:04 +0100

    second test

--- assets/js/sticky.js
+++ assets/js/sticky.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
-var navbar = document.getElementsByClassName("topnav");
+var navbar = document.getElementsById("topnav");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
